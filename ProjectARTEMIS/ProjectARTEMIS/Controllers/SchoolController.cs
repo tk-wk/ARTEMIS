42679f0b581734234e5aef00a654b4444d61aad5
bob <alexsamcabildo7@gmail.com>
2026-06-13 18:14:05 +0800

    Create SchoolController

---
+++ ProjectARTEMIS/ProjectARTEMIS/Controllers/SchoolController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectARTEMIS.Controllers
+{
+    [Route("api/v1/schools")]
+    [ApiController]
+    public class SchoolController : ControllerBase
+    {
+        private readonly SchoolService _schoolService;
+
+        public SchoolController(SchoolService schoolService) => _schoolService = schoolService;
+        
+        [HttpGet]
+        public async Task<IActionResult> GetSchools()
+        {
+            var result = await _schoolService.GetSchoolsAsync();
+
+            return Ok(result);
+        }
+    }
+}
