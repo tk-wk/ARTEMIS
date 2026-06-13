31edcf6e064e685034b7d09baf28d2a4226a78f6
bob <alexsamcabildo7@gmail.com>
2026-06-13 17:19:09 +0800

    Modify UserRepository to have GetByUsername()

--- ProjectARTEMIS/ProjectARTEMIS/Infrastructure/Repositories/UserRepository.cs
+++ ProjectARTEMIS/ProjectARTEMIS/Infrastructure/Repositories/UserRepository.cs
@@ -3,6 +3,7 @@
 public interface IUserRepository : IRepository<User>
 {
     // Add custom queries here later if needed (e.g., Task<User?> GetByUsernameAsync(string username);)
+    Task<User?> GetByUsernameAsync(string username);
 }
 
 public class UserRepository : Repository<User>, IUserRepository
@@ -10,4 +11,5 @@ public class UserRepository : Repository<User>, IUserRepository
     public UserRepository(MyDbContext context) : base(context)
     {
     }
+    public async Task<User?> GetByUsernameAsync(string username) => _set.FirstOrDefault(x=>x.Username == username);
 }
